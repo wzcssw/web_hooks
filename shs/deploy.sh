a5c1dcab131b5e2c2781e6d25dacb7f4a00819af
王志成 <wangzhicheng@tongxinyiliao.com>
2016-10-14 14:42:28 +0800

    a

--- shs/deploy.sh
+++ shs/deploy.sh
@@ -3,5 +3,5 @@ git branch -D master
 git fetch origin master:master
 git checkout master
 git branch -D temp_branch
-pm2 restart app.js
 echo "some data for the file" >> 'log/deploy.txt'
+pm2 restart app.js
