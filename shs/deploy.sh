6ea6577678aafe633e1de5acbe06a1ce8f67c1cf
王志成 <wangzhicheng@tongxinyiliao.com>
2016-10-14 16:10:48 +0800

    remove test sh

--- shs/deploy.sh
+++
@@ -1,7 +0,0 @@
-git checkout -b temp_branch
-git branch -D master
-git fetch origin master:master
-git checkout master
-git branch -D temp_branch
-echo "some data for the file" >> '~/log/web_hooks_deploy.txt'
-pm2 restart app.js
