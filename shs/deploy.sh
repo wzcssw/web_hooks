da7ea1738a961c45fd372767f933af176caed56e
王志成 <wangzhicheng@tongxinyiliao.com>
2016-10-14 14:46:13 +0800

    test

--- shs/deploy.sh
+++ shs/deploy.sh
@@ -3,5 +3,5 @@ git branch -D master
 git fetch origin master:master
 git checkout master
 git branch -D temp_branch
-echo "some data for the file" >> 'log/deploy.txt'
+echo "some data for the file" >> '~/log/web_hooks_deploy.txt'
 pm2 restart app.js
