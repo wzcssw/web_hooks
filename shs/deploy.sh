829e854ed84fbd60e7c34d3815a3588f19bc436b
王志成 <wangzhicheng@tongxinyiliao.com>
2016-10-14 14:38:49 +0800

    modify ignore

--- shs/deploy.sh
+++ shs/deploy.sh
@@ -1,5 +1,7 @@
-git checkout -b temp_brach
+git checkout -b temp_branch
 git branch -D master
 git fetch origin master:master
 git checkout master
-npm start
+git branch -D temp_branch
+pm2 restart app.js
+echo "some data for the file" >> 'log/deploy.txt'
